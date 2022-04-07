c29d6e9df53bf96b7a61e64ba77a37296f8f936b
Chris Marasco <cmarasco@aerosafeglobal.com>
2022-04-07 11:17:04 -0400

    Module 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
+import { map } from 'rxjs/add/operator/map';
 
 
 @Injectable()
@@ -10,7 +11,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._http.get(this._albumUrl).forEach(response => response.json);
+    return this._http.get(this._albumUrl);
   }
 
 }
